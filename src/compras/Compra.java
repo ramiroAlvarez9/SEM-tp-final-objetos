abe8d78ce547ce71fddcfef2ad7df8e42e275489
ramiroAlvarez9 <ramiro.al.alvarez@gmail.com>
2024-06-11 23:47:48 -0300

    testing sobre punto de venta

--- src/compras/Compra.java
+++ src/compras/Compra.java
@@ -6,32 +6,49 @@ import java.time.LocalTime;
 import sistema.PuntoDeVenta;
 
 public abstract class Compra {
-    static int lastID = 0;
-    protected int numControl;
-    protected LocalDate fecha;
-    protected LocalTime hora;
-    protected PuntoDeVenta puntoDeVenta;
+	static int lastID = 0;
+	protected int numControl;
+	protected LocalDate fecha;
+	protected LocalTime hora;
+	protected PuntoDeVenta puntoDeVenta;
 
-    public Compra(LocalDate fecha, LocalTime hora, PuntoDeVenta puntoDeVenta) {
-        this.numControl = lastID++;
-        this.fecha = fecha;
-        this.hora = hora;
-        this.puntoDeVenta = puntoDeVenta;
-    }
+	public Compra(LocalDate fecha, LocalTime hora, PuntoDeVenta puntoDeVenta) {
+		this.numControl = lastID++;
+		this.fecha = fecha;
+		this.hora = hora;
+		this.puntoDeVenta = puntoDeVenta;
+	}
 
-    public int getNumControl() {
-        return numControl;
-    }
+	public int getNumControl() {
+		return numControl;
+	}
 
-    public LocalDate getFechaCompra() {
-        return fecha;
-    }
+	public LocalDate getFechaCompra() {
+		return fecha;
+	}
 
-    public LocalTime getHora() {
-        return hora;
-    }
+	public LocalTime getHora() {
+		return hora;
+	}
 
-    public PuntoDeVenta getPuntoDeVenta() {
-        return puntoDeVenta;
-    }
+	public PuntoDeVenta getPuntoDeVenta() {
+		return puntoDeVenta;
+	}
+
+	// Hook method
+	public String getNumeroTel() {
+		// TODO Auto-generated method stub
+		return null;
+	};
+
+	// Hook method
+	public double getMonto() {
+		return 0;
+	};
+	
+	//Hook method
+	public int getCantHoras() {
+		return 0;
+	}
+
 }
--- test/sistema/PuntoDeVentaTest.java
+++ test/sistema/PuntoDeVentaTest.java
@@ -5,22 +5,50 @@ import static org.mockito.Mockito.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import aplicaciones.*;
+import compras.*;
 
+import static org.mockito.Mockito.*;
 
 class PuntoDeVentaTest {
-	
+
 	// Aplicacion(SEM sem, String patente, String numeroTel)
-	
+
+	String patente = "EWY-843";
+	String numTel = "11 5129-2345";
+	SEM sem;
 	PuntoDeVenta puntoDeVenta;
 	Aplicacion aplicacion;
-	
+
 	@BeforeEach
 	void setUp() throws Exception {
-	}
+		sem = mock(SEM.class);
+		puntoDeVenta = new PuntoDeVenta(sem);
 
+	}
+		
+	
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void testPuntoDeVentaAgregaCompraCorrectamenteCuandoCargaCredito() {
+		
+		//Prop: registra la compra dentro de su lista de compras (Compra por celular).
+		
+		puntoDeVenta.cargarCredito(numTel, 20.00);
+		
+		Compra compra = puntoDeVenta.getCompraPorNumeroDeTelefono(numTel); 
+		
+		assertEquals(numTel, compra.getNumeroTel());
+		
+	}
+	@Test
+	void testPuntoDeVentaRegistraCorrectamenteEstacionamientoPorCompraPuntual() {
+		//Prop: registra la compra dentro de su lista de compras (compra puntual, en "efectivo").
+
+		puntoDeVenta.registrarEstacionamiento(patente, 3);
+		
+		Compra compra = puntoDeVenta.getCompraPorPuntoDeVenta(puntoDeVenta);
+		
+		assertEquals(3, compra.getCantHoras());
+		assertEquals(puntoDeVenta, compra.getPuntoDeVenta());
 	}
 
 }
