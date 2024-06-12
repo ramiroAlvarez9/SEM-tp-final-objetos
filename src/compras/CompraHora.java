2c4700dff83dff4acc8ba743c7692dcbdd635fd3
ramiroAlvarez9 <ramiro.al.alvarez@gmail.com>
2024-06-11 22:23:21 -0300

    agrando compras

--- src/compras/CompraHora.java
+++ src/compras/CompraHora.java
@@ -3,11 +3,13 @@ package compras;
 import java.time.LocalDate;
 import java.time.LocalTime;
 
+import sistema.PuntoDeVenta;
+
 public class CompraHora extends Compra {
 	private int cantHoras;
 
-	public CompraHora(LocalDate fecha, LocalTime hora, int cantHoras) {
-		super(fecha, hora);
+	public CompraHora(LocalDate fecha, LocalTime hora, int cantHoras, PuntoDeVenta puntoDeVenta) {
+		super(fecha, hora, puntoDeVenta);
 		this.cantHoras = cantHoras;
 	}
 
--- src/sistema/PuntoDeVenta.java
+++ src/sistema/PuntoDeVenta.java
@@ -1,13 +1,24 @@
 package sistema;
+import compras.*;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
 
 public class PuntoDeVenta {
+
     private final SEM sem;
+    private List<Compra> comprasRealizadas = new ArrayList<Compra>();
 
     public PuntoDeVenta(SEM sem) {
-        this.sem = sem;
+        this.sem = sem; 
     }
 
-    public void cargarCredito(String numeroTel, Double credito) {
-        sem.cargarCredito(numeroTel, credito);
+    public void cargarCredito(String numeroTel, Double monto) {
+        sem.cargarCredito(numeroTel, monto);
+        CompraCelular nuevaCompra = new CompraCelular(LocalDate.now(), LocalTime.now(),numeroTel, monto, this);
+        comprasRealizadas.add(nuevaCompra);
     }
+
 }
